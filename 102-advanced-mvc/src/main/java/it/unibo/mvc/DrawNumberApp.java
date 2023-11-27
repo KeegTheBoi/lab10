04af49cc3103303906c0cdf9018944cd813b61f4
Keegan Carlo Falcao - keegancarlo.falcao@studio.unibo.it <keegancarlo.falcao@studio.unibo.it>
2023-11-27 13:31:58 +0100

    102 done

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,15 +1,20 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+    private static final URL DEFAULT_CONFIG_FILE = ClassLoader.getSystemResource("config.yml");
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
@@ -22,12 +27,36 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
         /*
          * Side-effect proof
          */
+        Configuration configuration = null;
         this.views = Arrays.asList(Arrays.copyOf(views, views.length));
         for (final DrawNumberView view: views) {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        try {
+            configuration = setCongfigElements(DEFAULT_CONFIG_FILE);
+        } catch (Exception e) {
+            for (final DrawNumberView view: views) {
+                view.displayError(e.getMessage());
+            } 
+        }
+        this.model = new DrawNumberImpl(configuration.getMin(), configuration.getMax(), configuration.getAttempts());
+        
+    }
+
+    private Configuration setCongfigElements(final URL yamlFile) throws FileNotFoundException, IOException{
+        Objects.requireNonNull(yamlFile);
+        final BufferedReader lines = new BufferedReader(new FileReader(new File(yamlFile.getFile())));
+        String line;
+        final List<Integer> values = new ArrayList<>();
+        while((line = lines.readLine()) != null){
+            values.add(Integer.parseInt(line.split(": ", 2)[1]));
+        }
+        Configuration.Builder configBuilder = new Configuration.Builder();
+        configBuilder.setMin(values.get(0));
+        configBuilder.setMax(values.get(1));
+        configBuilder.setAttempts(values.get(2));
+        return configBuilder.build();
     }
 
     @Override
@@ -66,7 +95,8 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
      * @throws FileNotFoundException 
      */
     public static void main(final String... args) throws FileNotFoundException {
-        new DrawNumberApp(new DrawNumberViewImpl());
+
+        new DrawNumberApp(new DrawNumberViewImpl(), new PrintStreamView(System.out));
     }
 
 }
